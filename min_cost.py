a55015cfb1fac61b55ce7f0bad316a5ce11eeb66
jomarydaison16 <Jomari.Daison@gmail.com>
2021-06-20 14:44:53 +0800

    added the file min_cost

---
+++ min_cost.py
@@ -0,0 +1,48 @@
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def getMinimumCost(k, c):
+	Cost = 0 #Initialize the total cost
+	NumPurchase = 0 #Initialize number of costumer's purchase
+
+	c.sort(reverse=True)
+
+	if n >= 0 and k > 0: #Check if the input is valid k should be 1 or more and n should not have negative value
+		if (n >= 0 and n <= k):
+			# print("first")
+			for i in range(n):
+				Cost += 1 * c[i]
+			return Cost
+		
+		elif(n > k):
+			# print("Second")
+			for i in range(n):
+				# print("i",i)
+				# print("n",n)
+				Cost += (NumPurchase + 1) * c[i]
+				# print("cost",Cost)
+				if (i+1)%k==0:
+					NumPurchase += 1
+			return Cost
+		
+		else:
+			# print("Last")
+			print("Please enter valid Input")
+	else:
+			print("Please enter valid Input")
+
+
+
+if __name__ == "__main__":
+	nk = input().split()
+	n = int(nk[0])
+	k = int(nk[1])
+	c = list(map(int, input().rstrip().split()))
+
+	print(getMinimumCost(k, c))
